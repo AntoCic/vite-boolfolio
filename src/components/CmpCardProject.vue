30f44c413b0d02ed1e73de544f08e7ad03e28430
AntoCic <anto.cic.127@gmail.com>
2024-06-14 20:16:44 +0200

    ADD: CmpCardProject

---
+++ src/components/CmpCardProject.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-3 p-3">
+    <RouterLink :to="{ name: 'project', params: { slug: project.slug } }"
+      class="d-block my-3 text-white border border-opacity-75 rounded-4 bg-white bg-opacity-10 p-2 h-100">
+      <p class="color-sub-title">{{ project.state }}</p>
+      <p class="text-truncate">{{ project.description }}</p>
+      <template v-if="project.type">
+        <span class="badge text-bg-primary me-1">{{ project.type.name }}</span>
+      </template>
+      <template v-if="project.technologies">
+        <span v-for="technology in project.technologies" :key="technology.id"
+          class="badge text-bg-warning text-white me-1">{{ technology.name }}</span>
+      </template>
+    </RouterLink>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  },
+}
+</script>
+
+
+<style lang="scss" scoped>
+.color-sub-title {
+  color: rgb(252, 232, 54);
+}
+</style>
--- src/pages/PortfolioView.vue
+++ src/pages/PortfolioView.vue
@@ -1,29 +1,16 @@
 <template>
    <div class="container">
       <div class="row text-center">
-         <div v-for="project in store.projects.data" :key="project.id" class="col-3 p-3">
-            <div class="my-3 text-white border border-opacity-75 rounded-4 bg-white bg-opacity-10 p-2 h-100">
-               <h3>{{ project.name }}</h3>
-               <p class="color-sub-title">{{ project.state }}</p>
-               <p class="text-truncate">{{ project.description }}</p>
-               <template v-if="project.type">
-                  <span class="badge text-bg-primary me-1">{{ project.type.name }}</span>
-               </template>
-               <template v-if="project.technologies">
-                  <span v-for="technology in project.technologies" :key="technology.id"
-                     class="badge text-bg-warning text-white me-1">{{ technology.name }}</span>
-               </template>
-               <RouterLink :to="{ name: 'project', params: {slug:project.slug} }" class="btn btn-outline-light">project</RouterLink>
-            </div>
-
-         </div>
+         <CmpCardProject v-for="project in store.projects.data" :key="project.id" :project="project"/>
       </div>
    </div>
 </template>
 
 <script>
 import { store } from '../store.js';
+import CmpCardProject from '../components/CmpCardProject.vue';
 export default {
+   components: { CmpCardProject},
    data() {
       return {
          store,
